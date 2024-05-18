1b5e42a0127d5b830d072b10b5fa11e9f06f2d9e
Katharina <katharinazlosa@gmail.com>
2024-05-18 18:53:09 +0200

    styles added

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,11 +1,14 @@
 import Footer from "./footer";
+import Form from "./form";
 import Header from "./header";
 
 const Contact = () => {
   return (
     <>
       <Header />
-      <div className="contact-container"></div>
+      <div className="contact-container">
+        <Form />
+      </div>
       <Footer />
     </>
   );
